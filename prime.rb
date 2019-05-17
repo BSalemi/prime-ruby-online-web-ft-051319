ef35ea78bc3e19a4034ef89f1062dd868b8f94a9
Brian Salemi <briandsalemi@gmail.com>
2019-05-17 14:35:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(number)
   n = (2..number-1).to_a
   return false if number <= 1
-  return true number == 2 || number == 3
+  return true if number == 2 || number == 3
   n.each do |integer|
     if integer % 2 == 0
       return false
@@ -10,4 +10,4 @@ def prime?(number)
       return true
     end
   end
-end 
+end
